e75a9a5972445384b9d12bcfb3c45d9e0cb23dde
Nikita Grigorev <pettyaelse@gmail.com>
2023-01-06 03:37:03 +0700

    implement TaskManager

--- src/TaskManager.h
+++ src/TaskManager.h
@@ -1,15 +1,41 @@
 #pragma once
 
+#include <vector>
+
 class TaskManager
 {
-	friend class Task;
-
 public:
-	Task createTask(int max_progress = 100, int current_progress = 0);
+	int createTask(int max_progress = 100, int current_progress = 0);
+	void removeTask(int task_id);
+
+	bool isTaskExists(int task_id) const;
+
+	int getTasksCount() const;
+	int getTaskId(int index) const;
+
+	void finishTask(int task_id);
+
+	void setCurrentProgress(int task_id, int value);
+	int getCurrentProgress(int task_id) const;
+
+	void setMaxProgress(int task_id, int max);
+	int getMaxProgress(int task_id) const;
 
 private:
-	void finish_task(int task_id);
+	struct Task
+	{
+		int task_id{-1};
+		int current_progress{0};
+		int max_progress{100};
+		bool is_finished{false};
+	};
 
 private:
+	int generate_task_id() const;
 
+	Task *get_task_by_id(int id);
+	const Task *get_task_by_id(int id) const;
+
+private:
+	std::vector<Task> tasks_;
 };
